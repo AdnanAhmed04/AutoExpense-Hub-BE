5d92368db84c465ab840e405279663ab1ed33e0d
AdnanAhmed04 <adnanb7208@gmail.com>
2026-03-03 04:35:16 +0500

    vercel backend setup try2

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config();
@@ -16,10 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/cars', require('./routes/carRoutes'));
-app.use('/api/expenses', require('./routes/expenseSingleRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/cars", require("./routes/carRoutes"));
+app.use("/api/expenses", require("./routes/expenseSingleRoutes"));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app; 
